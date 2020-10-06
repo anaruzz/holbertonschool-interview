c01e8173fccdd40876ba79a908a56f192b94ab6f
anaruzz <laabidigh@gmail.com>
2020-10-06 16:41:17 +0100

    WHY WONT YOU WORK!

--- 0-insert_number.c
+++ 0-insert_number.c
@@ -14,7 +14,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 listint_t *new, *temp;
 
-if (!head)
+if (head == NULL)
 return (NULL);
 
 new = malloc(sizeof(listint_t));
@@ -27,11 +27,10 @@ if (*head == NULL)
 {
 new->next = NULL;
 *head = new;
-return (new);
+return (*head);
 }
 
 temp = *head;
-
 if (new->n < temp->n)
 {
 new->next = *head;
