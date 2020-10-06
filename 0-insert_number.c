9c6c7256505a573988ad176c56cedee0de83d7d8
anaruzz <laabidigh@gmail.com>
2020-10-06 16:43:15 +0100

    try

--- 0-insert_number.c
+++ 0-insert_number.c
@@ -1,49 +1,38 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-* insert_node - insert number into a sorted signly linked list
-* @head: head of list
-* @number: number to be inserted
-* Return: Always 0.
-*/
+ * insert_node - Insert a node in a sorted linked list
+ * @head: Node
+ * @number: number to add
+ * Return: Address of new node
+ */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-listint_t *new, *temp;
+	listint_t *new_node = NULL, *p;
 
-if (head == NULL)
-return (NULL);
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
+	if (*head == NULL)
+		*head = new_node;
+	else if ((*head)->n > number)
+	{
+		new_node->next = *head;
+		*head = new_node;
 
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
-
-new->n = number;
-
-if (*head == NULL)
-{
-new->next = NULL;
-*head = new;
-return (*head);
-}
-
-temp = *head;
-if (new->n < temp->n)
-{
-new->next = *head;
-*head = new;
-return (*head);
-}
-
-while (temp->next != NULL && (new->n > temp->next->n))
-{
-temp = temp->next;
-}
-new->next = temp->next;
-temp->next = new;
-
-return (*head);
+	}
+	else
+	{
+		p = *head;
+		while (p->next != NULL && p->next->n < number)
+			p = p->next;
+		new_node->next = p->next;
+		p->next = new_node;
+	}
+return (new_node);
 }
