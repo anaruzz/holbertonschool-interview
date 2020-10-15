02146519f5df151a757e7c54fbed61a1fdabb2df
anaruzz <laabidigh@gmail.com>
2020-10-15 16:09:42 +0100

    palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* is_palindrome - check the code for Holberton School students.
+* @head: head of linked list
+* Return: 0 or 1.
+*/
+int is_palindrome(listint_t **head)
+{
+listint_t *first, *last;
+
+if (!(*head))
+return (0);
+
+last = *head;
+while (last->next)
+{
+last = last->next;
+}
+first = *head;
+while (last >= first)
+{
+if (first->n == last->n)
+{
+first = first->next;
+last -= 2;
+}
+else
+return (0);
+}
+return (1);
+}
