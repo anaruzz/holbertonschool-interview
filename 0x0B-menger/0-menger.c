de38b5947049479a21a7edeab1514f1051ee7b5a
anaruzz <laabidigh@gmail.com>
2021-03-04 00:04:02 +0100

    betty fix

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,38 +1,45 @@
 #include "stdio.h"
 #include "math.h"
 
+
+/**
+* Character - which char to print at an exact position
+* @i: line
+* @j: column
+* Return: character to print
+*/
 char Character(int i, int j)
 {
-  for (; i && j; i /= 3, j /= 3)
-  {
-    if ((i % 3 == 1) && (j % 3 == 1))
-      return (' ');
-  }
-  return ('#');
+for (; i && j; i /= 3, j /= 3)
+{
+if ((i % 3 == 1) && (j % 3 == 1))
+return (' ');
+}
+return ('#');
 }
 /**
 * menger - draws a 2D menger sponge
-* @level - level of the menger sponge to draw
+* @level: level of the menger sponge to draw
 */
 
 void menger(int level)
 {
-  int dim, i, j;
-  char c;
+int dim, i, j;
+char c;
 
 if (level >= 0)
 {
-  dim = pow(3, level);
+dim = pow(3, level);
 
-  for (i = 0; i < dim; i++)
-  {
-    for (j = 0; j < dim; j++)
-    {
-      c = Character(i, j);
-      printf("%c", c);
-    }
-    printf("\n");
-  }
+for (i = 0; i < dim; i++)
+{
+for (j = 0; j < dim; j++)
+{
+c = Character(i, j);
+printf("%c", c);
+}
+printf("\n");
+}
 
 
 }
