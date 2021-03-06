865d745c3c307797b1527559b30d60c5e9b883b7
anaruzz <laabidigh@gmail.com>
2021-03-06 15:46:55 +0100

    fix

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,46 +4,36 @@ A script that reads stdin line by line and computes metrics
 """
 import sys
 
-
-def addFileSize(data):
-    return int(data.split()[8])
-
-
-def codes(data):
-    return data.split()[7]
-
-counter = 0
-total_size = 0
-code_status = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-
+status = {"200": 0,
+          "301": 0,
+          "400": 0,
+          "401": 0,
+          "403": 0,
+          "404": 0,
+          "405": 0,
+          "500": 0
+          }
+i = 0
+total = 0
 try:
     for line in sys.stdin:
-        counter += 1
-        total_size += addFileSize(line)
-        code = codes(line)
-        if code in code_status:
-            code_status[code] += 1
-        if counter == 10:
-            print("File size: {:d}".format(total_size))
-            for code, value in sorted(code_status.items()):
-                if value > 0:
-                    print("{}: {:d}".format(code, value))
-            counter = 0
-except Exception:
+        i += 1
+        total += int(line.split()[8])
+        code = line.split()[7]
+        if code in status:
+            status[code] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(total))
+            for code, stat in sorted(status.items()):
+                if stat > 0:
+                    print("{}: {}".format(code, stat))
+                i = 0
+except Exception as E:
     pass
 
 finally:
-    print("File size: {:d}".format(total_size))
-    for code, value in sorted(code_status.items()):
-        if value > 0:
-            print("{}: {:d}".format(code, value))
-    counter = 0
+    print("File size: {}".format(total))
+    for code, stat in sorted(status.items()):
+        if stat > 0:
+            print("{}: {}".format(code, stat))
+    i = 0
