101f5f2a26611626aac4374dad7439213d7b70dd
anaruzz <laabidigh@gmail.com>
2021-03-06 13:29:12 +0100

    read stdin

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A script that reads stdin line by line and computes metrics
+"""
+import sys
+
+status = {"200": 0,
+          "300": 0,
+          "400": 0,
+          "401": 0,
+          "403": 0,
+          "404": 0,
+          "405": 0,
+          "500": 0}
+i = 0
+total = 0
+try:
+    for line in sys.stdin:
+        i += 1
+        total += int(line.split()[8])
+        code = line.split()[7]
+        if code in status:
+            status[code] += 1
+        if i == 10:
+            print("File size: {}".format(total))
+            for code, stat in status.items():
+                if stat > 0:
+                    print("{}: {}".format(code, stat))
+                i = 0
+except Exception as E:
+    pass
+
+finally:
+    print("File size: {}".format(total))
+    for code, stat in status.items():
+        if stat > 0:
+            print("{}: {}".format(code, stat))
+    i = 0
