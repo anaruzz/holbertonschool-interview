a6709732f5cc7f4ae2ab6c50712073400e5c41c2
anaruzz <laabidigh@gmail.com>
2021-04-04 21:19:24 +0100

    linear skip

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search.h"
+
+/**
+ * linear_skip - searches for a value in a skiplist
+ *
+ * @list: Pointer to the head of the list
+ * @value: value to search for
+ * Return: void
+ */
+ skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+  skiplist_t *tmp, *prev;
+
+if (!list)
+return (NULL);
+
+prev = list;
+tmp = list->express;
+while (tmp != NULL)
+{
+  printf("Value checked at index [%li] = [%d]\n", tmp->index, tmp->n);
+  if (tmp->express == NULL)
+  {
+    while (tmp->next)
+      tmp = tmp->next;
+    break;
+  }
+  tmp = tmp->express;
+}
+
+printf("Value found between indexes [%lu] and [%i]\n", prev->index, prev->n);
+
+while (prev && (prev->index <= tmp->index))
+{
+  printf("Value checked at index [%lu] = [%i]\n", prev->index, prev->n);
+  if (prev->n == value)
+  return (prev);
+  prev = prev->next;
+}
+return (NULL);
+}
