aac5bcb99b3001a9e1f8dca20e42517777050445
anaruzz <laabidigh@gmail.com>
2021-04-07 19:42:58 +0100

    try

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/*
+    A script that prints all characters of a Star Wars movie:
+    The first positional argument passed is the Movie ID -
+
+*/
+
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
+    }
+  }
+});
