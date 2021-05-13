47da0f96e1f3d94e7a42a1fca356964cdec26135
anaruzz <laabidigh@gmail.com>
2021-05-14 00:04:25 +0100

    betty

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -3,16 +3,16 @@
 
 #include "binary_trees.h"
 
-/*
- * heap_extract - extracts root of MBH
- * @root - root of the node
- * Return: value stored
- */
- int heap_extract(heap_t **root)
- {
-   heap_t *tmp;
-   tmp = *root;
+/**
+* heap_extract - extracts root of MBH
+* @root: root of the node
+* Return: value stored
+*/
+int heap_extract(heap_t **root)
+{
+heap_t *tmp;
+tmp = *root;
 if (tmp)
-return 0;
-return 0;
- }
+return (0);
+return (0);
+}
