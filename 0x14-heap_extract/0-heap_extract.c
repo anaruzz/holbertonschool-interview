906306e24009bf2b3a7ea81c909754dcf462f997
anaruzz <laabidigh@gmail.com>
2021-05-14 00:01:44 +0100

    first commit

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "binary_trees.h"
+
+/*
+ * heap_extract - extracts root of MBH
+ * @root - root of the node
+ * Return: value stored
+ */
+ int heap_extract(heap_t **root)
+ {
+   heap_t *tmp;
+   tmp = *root;
+if (tmp)
+return 0;
+return 0;
+ }
