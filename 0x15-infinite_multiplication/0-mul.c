a08f2091d667ed676339b05aba9766573c171502
anaruzz <laabidigh@gmail.com>
2021-06-14 15:37:22 +0100

    fix

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,8 +1,19 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "holberton.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
 * is_number - prints Error
@@ -25,7 +36,7 @@ return (1);
 *
 * Return: void
 */
-void print_error()
+void print_error(void)
 {
 int i;
 char *err = "Error\n";
@@ -196,56 +207,3 @@ print_array(res, len_a + len_b);
 
 return (0);
 }
-=======
-#include <unistd.h>
-#include "holberton.h"
-
-/**
-* is_number - prints Error
-* @s: string passed for testing
-* Return: 1 or 0 on failure
-*/
-int is_number(char *s)
-{
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] > '9' || s[i] < '0')
-return (0);
-}
-return (1);
-}
-
-/**
-* main - multiplies two positive numbers
-* @argc: argument counter
-* @argv: argument vector
-* Return: 0
-*/
-int main(int argc, char *argv[])
-{
-int *res, len_a, len_b, *a, *b;
-
-if (argc != 3 || !is_number(argv[1]) || !is_number(argv[2]))
-{
-print_error();
-exit(98);
-}
-
-len_a = _strlen(argv[1]);
-len_b = _strlen(argv[2]);
-
-a = to_array(argv[1]);
-b = to_array(argv[2]);
-
-
-a = reverse_arr(a, len_a);
-b = reverse_arr(b, len_b);
-
-
-res = multiply(a, b, len_a, len_b);
-print_array(res, len_a + len_b);
-
-return (0);
-}
->>>>>>> 560ecd46cd91869874a8ec84e6c4f749b4fc815d
