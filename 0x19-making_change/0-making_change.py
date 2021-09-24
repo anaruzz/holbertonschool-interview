795b9ded8f187aee031c0b4a0760492875276669
anaruzz <laabidigh@gmail.com>
2021-09-24 05:24:35 +0100

    fix issue

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A function, that given a pile of coins of different values,
+determines the fewest number of coins needed to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """
+    Returns sum
+    """
+    sum = 0
+    if (total <= 0):
+        return 0
+    coins.sort(reverse=True)
+    for i in coins:
+        if (total < i):
+            pass
+        q, r = divmod(total, i)
+        total = r
+        sum += q
+    if (total != 0):
+        return -1
+    return sum
